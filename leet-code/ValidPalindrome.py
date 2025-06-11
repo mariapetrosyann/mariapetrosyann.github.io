8e17bad6296a69ec1c4e3627b10c8616302aba44
Maria Petrosyan <mariaapetrosian@gmail.com>
2025-06-11 14:54:37 +0400

    Update ValidPalindrome.py

--- leet-code/ValidPalindrome.py
+++ leet-code/ValidPalindrome.py
@@ -2,8 +2,9 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         word = s.lower()
         new_word = word.replace(" ","").replace(",", "").replace(":","").replace(".", "")
-        if new_word == new_word[::-1] and new_word == "":
+        if new_word == "":
+            return True
+        elif new_word == new_word[::-1]:
             return True     
         else:
             return False
-        
