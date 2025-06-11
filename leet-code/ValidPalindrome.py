f50295159e778c0e5f179c4c15c4f394bb1e22d6
Maria Petrosyan <mariaapetrosian@gmail.com>
2025-06-11 14:51:52 +0400

    Create ValidPalindrome.py

---
+++ leet-code/ValidPalindrome.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        word = s.lower()
+        new_word = word.replace(" ","").replace(",", "").replace(":","").replace(".", "")
+        if new_word == "":
+            return True
+        if new_word == new_word[::-1]:
+            return True     
+        else:
+            return False
+        
