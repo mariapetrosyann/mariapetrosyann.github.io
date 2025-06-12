c88cd23993148e81dfd46d1d1ff069a3f30075a4
Maria Petrosyan <mariaapetrosian@gmail.com>
2025-06-12 12:06:05 +0400

    Update WordPattern.py

--- leet-code/WordPattern.py
+++ leet-code/WordPattern.py
@@ -1,7 +1,6 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         map_pattern_to_s = {}
-        map_s_to_pattern = {}
         pattern=[ch for ch in pattern]
         s=s.split()
         if len(pattern) != len(s):
@@ -10,10 +9,5 @@ class Solution:
             if chars in map_pattern_to_s:
                 if map_pattern_to_s[chars] != words:
                     return False
-            if words in map_s_to_pattern:
-                if map_s_to_pattern[words] != chars:
-                    return False
             map_pattern_to_s[chars] = words
-            map_s_to_pattern[words] = chars
         return True
-      
